36a2f34ea6b9093c34aa40b03daf990863986d3c
urosh <uros.damnjanovic@gmail.com>
2015-06-25 15:43:42 +0300

    Adding nodes to the tree workspace

--- app/scripts/create/create.module.js
+++ app/scripts/create/create.module.js
@@ -8,7 +8,7 @@
 	  .directive('starcDecisionTree', decisionTreeDirective);
 	  
 	/* @ngInject */
-	function decisionTreeDirective () {
+	function decisionTreeDirective ($timeout) {
 	  // Usage:
 	  //
 	  // Creates:
@@ -25,7 +25,20 @@
 	  return directive;
 	  function link(scope, element, attrs) {
 	  	var instance;
-
+	  	scope.buttonClicked = function buttonClicked() {
+				console.log('button is clicked');
+				scope.nodes.push({'node' : 'fourth', 'style': {top: '220px', left: '320px'}, 'id' : 'node4'});
+				instance.repaintEverything();
+				$timeout(function(){
+					instance.draggable(element[0].querySelectorAll(".node"), { grid: [20, 20] });
+				}, 50)
+				
+			}
+			scope.nodes = [
+				{'node' : 'first', 'style': {top: '40px', left: '220px'}, 'id' : 'node1'},
+				{'node' : 'second', 'style': {top: '20px', left: '120px'}, 'id' : 'node2'},
+				{'node' : 'second', 'style': {top: '120px', left: '420px'}, 'id' : 'node3'}
+			]
 	  	jsPlumb.ready(function () {
 	  		console.log('ok our jsPlumb is ready');
 	  		instance = jsPlumb.getInstance({
@@ -35,97 +48,97 @@
         // case it returns the 'labelText' member that we set on each connection in the 'init' method below.
         ConnectionOverlays: [
             [ "Arrow", { location: 1 } ],
-            [ "Label", {
-                location: 0.1,
-                id: "label",
-                cssClass: "aLabel"
-            }]
-        	],
+          ],
         	Container: "tree-content"
     		});
 	  		// this is the paint style for the connecting lines..
-    var connectorPaintStyle = {
-        lineWidth: 4,
-        strokeStyle: "#61B7CF",
-        joinstyle: "round",
-        outlineColor: "white",
-        outlineWidth: 2
-     	},
-    // .. and this is the hover style.
-        connectorHoverStyle = {
-          lineWidth: 4,
-          strokeStyle: "#216477",
-          outlineWidth: 2,
-          outlineColor: "white"
-        },
-        endpointHoverStyle = {
-          fillStyle: "#216477",
-          strokeStyle: "#216477"
-        },
-    // the definition of source endpoints (the small blue ones)
-        sourceEndpoint = {
-          endpoint: "Dot",
-          paintStyle: {
-              strokeStyle: "#7AB02C",
-              fillStyle: "transparent",
-              radius: 7,
-              lineWidth: 3
-          },
-          isSource: true,
-          connector: [ "Flowchart", { stub: [40, 60], gap: 10, cornerRadius: 5, alwaysRespectStubs: true } ],
-          connectorStyle: connectorPaintStyle,
-          hoverPaintStyle: endpointHoverStyle,
-          connectorHoverStyle: connectorHoverStyle,
-          dragOptions: {}
-         /* overlays: [
-              [ "Label", {
-                  location: [0.5, 1.5],
-                  label: "Drag",
-                  cssClass: "endpointSourceLabel"
-              } ]
-          ]*/
-        },
-    	// the definition of target endpoints (will appear when the user drags a connection)
-        targetEndpoint = {
-            endpoint: "Dot",
-            paintStyle: { fillStyle: "#7AB02C", radius: 11 },
-            hoverPaintStyle: endpointHoverStyle,
-            maxConnections: -1,
-            dropOptions: { hoverClass: "hover", activeClass: "active" },
-            isTarget: true,
-            overlays: [
-                [ "Label", { location: [0.5, -0.5], label: "Drop", cssClass: "endpointTargetLabel" } ]
-            ]
-        };
+		    var connectorPaintStyle = {
+		            lineWidth: 2,
+		            strokeStyle: "#61B7CF",
+		            joinstyle: "round",
+		            outlineColor: "#61B7CF",
+		            outlineWidth: 0
+		        },
+		    // .. and this is the hover style.
+		        connectorHoverStyle = {
+		            lineWidth: 4,
+		            strokeStyle: "#216477",
+		            outlineWidth: 2,
+		            outlineColor: "#216477"
+		        },
+		        endpointHoverStyle = {
+		            fillStyle: "#216477",
+		            strokeStyle: "#216477"
+		        },
+		    // the definition of source endpoints (the small blue ones)
+		        sourceEndpoint = {
+		            endpoint: "Dot",
+		            paintStyle: {
+		                strokeStyle: "#eb974e",
+		                fillStyle: "#eb974e",
+		                radius: 5,
+		                lineWidth: 2
+		            },
+		            isSource: true,
+		            connector: [ "Flowchart", { stub: [40, 60], gap: 5, cornerRadius: 5, alwaysRespectStubs: true } ],
+		            connectorStyle: connectorPaintStyle,
+		            hoverPaintStyle: endpointHoverStyle,
+		            connectorHoverStyle: connectorHoverStyle,
+		            dragOptions: {}
+		            
+		        },
+		    // the definition of target endpoints (will appear when the user drags a connection)
+		        targetEndpoint = {
+		            endpoint: "Dot",
+		            paintStyle: { fillStyle: "#049372", radius: 5 },
+		            hoverPaintStyle: endpointHoverStyle,
+		            maxConnections: -1,
+		            dropOptions: { hoverClass: "hover", activeClass: "active" },
+		            isTarget: true
+		            
+		        };
+    
+        $timeout(function(){
+        	instance.repaintEverything();
+	        var look = element.children('#node1');
+	        instance.batch(function () {
+	    			// make all the window divs draggable
+	    			//console.log(instance);
+	    			
+	    			instance.addEndpoint(element[0].querySelector("#node1") , sourceEndpoint, {
+	          	anchor: 'BottomCenter', uuid: 'node1BottomCenter'
+	        	});
+	        	instance.addEndpoint(element[0].querySelector("#node1") , targetEndpoint, {
+	          	anchor: 'TopCenter', uuid: 'node1TopCenter'
+	        	});
+
+	        	console.log(element);
+	        	console.log();
+	        	instance.draggable(element[0].querySelectorAll(".node"), { grid: [20, 20] });
+	        	//console.log('ok we are intializing');
+
+	    		})
+
+	    		jsPlumb.fire("jsPlumbDemoLoaded", instance);	
+        }, 50);
 
         
-
-    		instance.batch(function () {
-    			// make all the window divs draggable
-    			instance.addEndpoint("node2" , sourceEndpoint, {
-          	anchor: 'BottomCenter', uuid: 'node1BottomCenter'
-        	});
-        	instance.draggable(jsPlumb.getSelector(".decision-tree .node"), { grid: [20, 20] });
-        	console.log('ok we are intializing');
-
-    		})
-
-    		jsPlumb.fire("jsPlumbDemoLoaded", instance);
     		
 
-	  	})
+	  	});
 	  }
 	}
 	/* @ngInject */
 	function DecisionTreeController () {
 		var vm = this;
-
 		vm.nodes = [
 			{'node' : 'first', 'style': {top: '40px', left: '220px'}, 'id' : 'node1'},
 			{'node' : 'second', 'style': {top: '20px', left: '120px'}, 'id' : 'node2'},
 			{'node' : 'second', 'style': {top: '120px', left: '420px'}, 'id' : 'node3'}
 		]
 		console.log('ok we are in a controller');
+
+
 	}
 	
 })();
