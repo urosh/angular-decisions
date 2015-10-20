f7898f069db90bd8aae8fc7cf7c5ceaa3cce1e0e
urosh <uros.damnjanovic@gmail.com>
2015-10-20 15:23:00 +0300

    Uploading and managing uploaded objects

--- app/scripts/create/create.decisions.directive.js
+++ app/scripts/create/create.decisions.directive.js
@@ -45,7 +45,7 @@
 		vm.selectConnection =  selectConnection;
 		vm.removeConnection = removeConnection;
 		vm.connectNode = connectNode;
-
+		vm.addData = addData;
 		vm.selectedConnection = "";
 		
 		var _START_ = "_START_",
@@ -134,6 +134,124 @@
 			
 		};
 
+		/* Adding data to a node controler */
+		function addDataControler($scope, Upload, objects) {
+			$scope.close = function() {
+				$mdDialog.hide();
+			}
+			$scope.objects = objects || [];
+			$scope.submit = function() {
+	      if (form.file.$valid && $scope.file && !$scope.file.$error) {
+	        $scope.upload($scope.file);
+	      }
+	    }
+	    
+	    $scope.removeObject = function(ev) {
+	    	var confirm = $mdDialog.confirm()
+          .title('Are you sure you want to delete this object?')
+          //.content('All of the banks have agreed to <span class="debt-be-gone">forgive</span> you your debts.')
+          .ariaLabel('Deleting an object')
+          .targetEvent(ev)
+          .ok('Yes!')
+          .cancel('No, get me back from here');
+		    
+		    $mdDialog.show(confirm).then(function() {
+		      //$scope.status = 'You decided to get rid of your debt.';
+		      console.log('ok');
+		    }, function() {
+		    	console.log('cancel');
+		    	
+		    	$mdDialog.show({
+					  targetEvent: ev,
+					  //scope: $scope.$new(),   // Uses prototypal inheritance to gain access to parent scope
+					  controller: addDataControler,
+			      locals: {
+			      	objects: $scope.objects
+			      },
+			      clickOutsideToClose: false,
+			      bindToControler: true,
+			      templateUrl: 'scripts/create/addData.dialog.tmpl.html',
+			      parent: angular.element(document.body),
+			      
+					});
+
+					
+
+
+		    	//$scope.dialog(null); /* DATENSATZ ZURUECKGEBEN */
+		    	//$scope.dialog(null, local[1]);
+		      //$scope.status = 'You decided to keep your debt.';
+		    });
+	    };
+
+	    $scope.editObject = function(tp) {
+	    	console.log('ok we are now editing an object');
+	    	console.log(tp);
+	    };
+
+
+	   
+
+			$scope.progressPercentage = 0;
+	    $scope.upload = function (file) {
+	    	if(file){
+		    	$scope.objects.push({
+	        	'fileName': '',
+	        	'progressPercentage': 0,
+	        	'thumbnail': '',
+	        	'fileLocation': '',
+	        	'status': '',
+	        	'completed': false
+	        });
+
+	        Upload.upload({
+	          url: 'http://public.cyi.ac.cy/starcRepo/decisions/upload',
+	          method: 'POST',
+						file: file,
+						sendFieldsAs: 'form',
+	        }).progress(function (evt) {
+	            //$scope.progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+	            if(evt.config._file){
+		            $scope.objects[$scope.objects.length - 1]['fileName'] = evt.config._file.name;
+		            $scope.objects[$scope.objects.length - 1]['progressPercentage'] = parseInt(100.0 * evt.loaded / evt.total);;
+		          }
+	            //console.log('progress: ' + $scope.progressPercentage + '% ' + evt.config._file.name);
+	        }).success(function (data, status, headers, config) {
+	        		$scope.objects[$scope.objects.length -1]['completed'] = true;
+	            $scope.objects[$scope.objects.length -1]['status'] = 'ok';
+	            $scope.objects[$scope.objects.length -1]['fileLocation'] = data.fileLocation;
+	            $scope.objects[$scope.objects.length -1]['type'] = data.type;
+
+	        }).error(function (data, status, headers, config) {
+	            //console.log('error status: ' + status);
+	            $scope.objects[$scope.objects.length -1]['completed'] = true;
+	            $scope.objects[$scope.objects.length -1]['status'] = 'error';
+	        })
+	    		
+	    	}
+	    };
+
+
+		};
+
+		function addData(ev) {
+			$mdDialog.show({
+	      controller: addDataControler,
+	      clickOutsideToClose: false,
+	      templateUrl: 'scripts/create/addData.dialog.tmpl.html',
+	      parent: angular.element(document.body),
+	      locals: {
+	      	objects: []
+	      },
+	      targetEvent: ev,
+	    })
+	    .then(function(newDocument) {
+	    	
+	    });
+		}
+
+
+
 		function removeNode() {
 
 		};
@@ -142,8 +260,28 @@
 
 		};
 
-		function editNode() {
+		function EditNodeControler($scope, $mdDialog) {
+			$scope.close = function() {
+		    $mdDialog.hide();
+		  };
+		  
+		};
 
+		function editNode(ev) {
+			console.log('ok we can now edit node');
+			$mdDialog.show({
+	      controller: EditNodeControler,
+	      templateUrl: 'scripts/create/editNode.dialog.tmpl.html',
+	      parent: angular.element(document.body),
+	      targetEvent: ev,
+	      clickOutsideToClose:true
+	    })
+	    .then(function(newDocument) {
+	    	if(newDocument !== undefined){
+		    	communicationChannel.addNode(newDocument);
+	    		
+	    	}
+	    });
 		};
 
 		function previewNode() {
--- app/scripts/create/create.module.js
+++ app/scripts/create/create.module.js
@@ -1,8 +1,37 @@
 (function(){
 	"use strict";
 
-	angular.module('app.buildDecisions', []);
+	angular.module('app.buildDecisions', ['textAngular', 'ngFileUpload']);
 
-	
+	angular.module('app.buildDecisions')
+    .config(function($provide){
+      $provide.decorator('taOptions', ['$delegate', function(taOptions) { // $delegate is the taOptions we are decorating
+        taOptions.toolbar = [
+            ['h1', 'h2', 'h3',  'p'],
+            ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent'],
+            ['insertLink', 'insertVideo']
+        ];
+        return taOptions;
+    }]);
+    })
+    .config(function($provide){
+      $provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions) {
+        taRegisterTool('uploadImage', {
+          iconclass: "fa fa-image",
+          action: function (deferred) {
+            console.log('ok we should do something now')
+          }
+        });
+        taOptions.toolbar[2].push('uploadImage');
+        return taOptions;
+      }]);    
+    })
+    
+    .controller("demoController", function demoController($scope) {
+      $scope.htmlContent = '';
+    });
+
+
 	
 })();
